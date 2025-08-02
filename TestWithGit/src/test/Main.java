cc5ba6a400dc8b9c40dd6ac12009826e4a589f56
thilina <thilinaudayanga1010@gmail.com>
2025-08-02 23:29:55 +0530

    create new branch

--- TestWithGit/src/test/Main.java
+++ TestWithGit/src/test/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world");
-	}
+		System.out.println("what is your name");
+		
+		int x = 1;
+		if(x == 1)
+			System.out.println("Enter name :");
+	};
 
 }
